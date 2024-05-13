85f0e62240119b66a477622b99b30e08fa3368a8
cmdax <cmdax@proton.me>
2024-05-13 10:55:46 +0200

    Add function getHumanChoice()

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+function getHumanChoice(){
+    return prompt('Choose between rock (r), paper (p) or scissors (s)').slice(0).toLowerCase();
+}
 //  CREATE a value for the player score
 let playerScore;
 
@@ -9,7 +12,7 @@ for (nbPartie = 0; nbPartie < 6; nbPartie++){
 
 //      ASK the player for a choice
     let playerChoice = getHumanChoice();
-
+    console.log(playerChoice);
 //      ASK the machine for a choice
     let machineChoice = getMachineChoice();
     
@@ -17,8 +20,10 @@ for (nbPartie = 0; nbPartie < 6; nbPartie++){
 //      INCREASE the score of the winner
     if (machineChoice > playerChoice) {
         machineScore++;
+        console.log('You lose!');
     } else if (machineChoice < playerChoice) {
         playerChoice++;
+        console.log('You win!');
     }
 }
 //  SAY it is over and show the score
