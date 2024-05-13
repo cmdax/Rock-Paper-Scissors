d149ecac61351c06c328f9692e235fd32035fc62
cmdax <cmdax@proton.me>
2024-05-13 10:24:11 +0200

    Add pseudocode

---
+++ script.js
@@ -0,0 +1,8 @@
+//  CREATE a value for the player score
+//  CREATE a value for the machine score
+//  FOR 5 rounds play the game
+//      ASK the player for a choice
+//      ASK the machine for a choice
+//      COMPARE the result
+//      INCREASE the score of the winner
+//  SAY it is over and show the score
