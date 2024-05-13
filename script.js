49ca3f3c502422e2274933017784e575d35a98b8
cmdax <cmdax@proton.me>
2024-05-13 10:42:34 +0200

    Create first skeletton of code

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 //  CREATE a value for the player score
+let playerScore;
+
 //  CREATE a value for the machine score
+let machineScore;
+
 //  FOR 5 rounds play the game
+for (nbPartie = 0; nbPartie < 6; nbPartie++){
+
 //      ASK the player for a choice
+    let playerChoice = getHumanChoice();
+
 //      ASK the machine for a choice
+    let machineChoice = getMachineChoice();
+    
 //      COMPARE the result
 //      INCREASE the score of the winner
+    if (machineChoice > playerChoice) {
+        machineScore++;
+    } else if (machineChoice < playerChoice) {
+        playerChoice++;
+    }
+}
 //  SAY it is over and show the score
+if (playerScore > machineScore){
+    console.log('You win the match!');
+} else if (playerScore < machineScore) {
+    console.log('You lose the match!');
+} else {
+    console.log('You tied the match!');
+}
