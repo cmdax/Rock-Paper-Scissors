816be860c830a98350e7a14975462b2df74282be
cmdax <cmdax@proton.me>
2024-05-13 11:23:31 +0200

    Add the function compareResults()

--- script.js
+++ script.js
@@ -1,6 +1,44 @@
 function getHumanChoice(){
     return prompt('Choose between rock (r), paper (p) or scissors (s)').slice(0).toLowerCase();
 }
+
+function getMachineChoice(){
+    choice = Math.random();
+    if (choice < 0.33) {
+        return 'r';
+    } else if (choice < 0.66) {
+        return 'p';
+    } else {
+        return 's';
+    }
+}
+
+function compareResults(machineChoice, playerChoice) {
+    let playerWin;
+
+    if (machineChoice != playerChoice) {
+        if (machineChoice == 'r' && playerChoice == 's'){
+            playerWin = false;
+        } else if (machineChoice == 's' && playerChoice == 'p') {
+            playerWin = false;
+        } else if (machineChoice == 'p' && playerChoice == 'r') {
+            playerWin = false;
+        } else { 
+            playerWin = true;
+        }
+    }
+    console.log(`playerWin = ${playerWin}`);
+
+    if (playerWin) {
+        playerScore++;
+        console.log('You win!');
+    } else if (playerWin == undefined) {
+        console.log('You tied.');
+    } else {
+        machineScore++;
+        console.log('You loose!');
+    }
+}
 //  CREATE a value for the player score
 let playerScore;
 
@@ -15,16 +53,11 @@ for (nbPartie = 0; nbPartie < 6; nbPartie++){
     console.log(playerChoice);
 //      ASK the machine for a choice
     let machineChoice = getMachineChoice();
-    
+    console.log(machineChoice); 
+
 //      COMPARE the result
 //      INCREASE the score of the winner
-    if (machineChoice > playerChoice) {
-        machineScore++;
-        console.log('You lose!');
-    } else if (machineChoice < playerChoice) {
-        playerChoice++;
-        console.log('You win!');
-    }
+    compareResults(machineChoice, playerChoice);
 }
 //  SAY it is over and show the score
 if (playerScore > machineScore){
